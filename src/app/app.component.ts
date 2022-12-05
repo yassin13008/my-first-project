156c2ebdd5d6e5ad8c79b2e1865b82f352839f34
Yassin Hammami <yas.hammami@gmail.com>
2022-12-05 10:58:14 +0100

    On a mis ces éléments en franças et crée une anima

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,55 @@
 import { Component, OnInit, } from '@angular/core';
+import { from } from 'rxjs';
+import { animate, state, style, transition, trigger } from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent implements OnInit{
-  title="my-first-project"
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style(
+          {
+            height: '200px',
+            opacity: 1,
+            backgroundColor: 'Yellow'
+          }
+        )
+      ),
+      state(
+        'closed',
+        style(
+          {
+            height: '100px',
+            opacity: 0.8,
+            backgroundColor: 'Blue'
+          }
+        )
+      ),
+      transition('open => closed', [animate('1s')]),
+      transition('closed => open', [animate('1s')])
+    ])
+  ]
 
-ngOnInit(): void {
-  this.title = 'nouveau title'
-  console.log('Salut ngoninit')
-}
-  onChildMessage(args : string) {
+})
+export class AppComponent implements OnInit {
+  title = "my-first-project"
+
+  cuisine: string = '';
+
+  isOpen= false;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  ngOnInit(): void {
+    this.title = 'nouveau title'
+    console.log('Salut ngoninit')
+  }
+  onChildMessage(args: string) {
     console.log(args);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 // import { HttpClienModule } from "@angular/common/http";
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from 'src/services/in-memory-data.service';
 // import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from './app.component';
 import { FirstCpmComponent } from './first-cpm/first-cpm.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+
+registerLocaleData(localeFr, 'fr')
 
 @NgModule({
   declarations: [
@@ -18,9 +24,18 @@ import { FirstCpmComponent } from './first-cpm/first-cpm.component';
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
     InMemoryDataService, { dataEncapsulation : false }
-    )
+    ),
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: 
+  [
+    {
+      provide: LOCALE_ID, useValue: 'fr'
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
